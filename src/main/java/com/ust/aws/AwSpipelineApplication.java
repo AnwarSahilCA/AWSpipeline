6356068d0d5cc14d4b521c8b21d9e5cae00c3b37
AnwarSahilCA <245085@ust.com>
2023-06-03 00:38:22 +0530

    firstcommit

---
+++ src/main/java/com/ust/aws/AwSpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.ust.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwSpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwSpipelineApplication.class, args);
+	}
+
+}
